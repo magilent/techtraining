4d3af52c93bb4607d4930155007fb47b053f095b
John Fandl <johnfandl@gmail.com>
2020-02-04 00:28:44 +0000

    My first commit

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "openacademy",
+
+    'summary': """
+        John Fandl Test Module for Technical Training""",
+
+    'description': """
+        Open Acacdemy Test Module
+    """,
+
+    'author': "mAgilEnt",
+    'website': "http://www.magilent.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
